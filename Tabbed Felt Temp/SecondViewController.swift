5e8827e65bf9a7842901c6f843723cbb74ec89cc
HS <sekhah@my.lanecc.edu>
2020-02-21 07:53:36 -0800

    Initial Commit

---
+++ Tabbed Felt Temp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Tabbed Felt Temp
+//
+//  Created by Wolfgang on 2/21/20.
+//  Copyright © 2020 Wolfgang's Software Factory. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
